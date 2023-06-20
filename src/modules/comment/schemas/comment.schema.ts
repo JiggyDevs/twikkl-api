a2e3af45f19cdc0a1f58e0e12ec3c392e7dd3b92
Prince Codes <princecodes247@gmail.com>
2023-06-20 07:05:13 +0100

    add category and comment module

---
+++ src/modules/comment/schemas/comment.schema.ts
@@ -0,0 +1,28 @@
+import { User } from '../../user/schemas/user.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument, Types } from 'mongoose';
+
+export type GroupDocument = HydratedDocument<Group>;
+
+@Schema()
+export class Group {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  body: string;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  createdBy: User;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
+  members: User[];
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+
+  @Prop({ default: false })
+  isAdminDeleted: boolean;
+}
+
+export const GroupSchema = SchemaFactory.createForClass(Group);
