be195969076672214d7b59fc9383138913b3178f
Prince Codes <princecodes247@gmail.com>
2023-09-05 22:36:48 +0100

    fix: username required in user model

--- src/modules/user/schemas/user.schema.ts
+++ src/modules/user/schemas/user.schema.ts
@@ -5,7 +5,7 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop({ required: true, unique: true })
+  @Prop({ default: "", unique: true })
   username: string;
 
   @Prop({ required: true, unique: true })
