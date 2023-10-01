449e4432f658716892cd167244706e41c0fe58ab
niel98 <mosesdaniel201798@gmail.com>
2023-10-01 21:10:06 +0100

    fix: fixed get all users and populate option

--- src/core/abstracts/generic-repository.abstract.ts
+++ src/core/abstracts/generic-repository.abstract.ts
@@ -1,18 +1,35 @@
-import { ClientSession, FilterQuery, UpdateQuery } from 'mongoose'
+import { ClientSession, FilterQuery, UpdateQuery } from 'mongoose';
 
 export abstract class IGenericRepository<T> {
-    
-    abstract find(fields: FilterQuery<T>, options?: { select?: string, isLean?: boolean })
+  abstract find(
+    fields: FilterQuery<T>,
+    options?: {
+      select?: string;
+      isLean?: boolean;
+      populate?: string | string[];
+    },
+  );
 
-    abstract findAllWithPagination(query: any, options?: { fields?: FilterQuery<T>, populate?: string | string[] })
+  abstract findAllWithPagination(
+    query: any,
+    options?: { fields?: FilterQuery<T>; populate?: string | string[] },
+  );
 
-    abstract findOne(key: FilterQuery<T>, session?: ClientSession, options?: { select?: string | string[], populate?: string | string[] })
+  abstract findOne(
+    key: FilterQuery<T>,
+    session?: ClientSession,
+    options?: { select?: string | string[]; populate?: string | string[] },
+  );
 
-    abstract create(payload: T | T[], session?: ClientSession)
+  abstract create(payload: T | T[], session?: ClientSession);
 
-    abstract update(key: FilterQuery<T>, payload: UpdateQuery<T>, session?: ClientSession)
+  abstract update(
+    key: FilterQuery<T>,
+    payload: UpdateQuery<T>,
+    session?: ClientSession,
+  );
 
-    abstract delete(key: FilterQuery<T>, session?: ClientSession)
+  abstract delete(key: FilterQuery<T>, session?: ClientSession);
 
-    abstract search(options: Record<string, any>, populate: string | string[])
+  abstract search(options: Record<string, any>, populate: string | string[]);
 }
